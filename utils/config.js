a0418a10c521945df5cc2553aa2d89169d8f1fa4
xuxiwang <1421303849@qq.com>
2023-08-03 11:17:41 +0800

    fix：init

--- utils/config.js
+++ utils/config.js
@@ -1,25 +1,24 @@
-// 应用全局配置
+// 通用应用全局配置文件
 module.exports = {
-  baseUrl: 'https://lightning.zhijianiot.com/api',
-  // baseUrl: 'http://localhost:8109',
+  baseUrl: 'https://www.xxx.com',
   // 应用信息
   appInfo: {
     // 应用名称
-    name: "ruoyi-app",
+    name: "xxx",
     // 应用版本
     version: "1.1.0",
     // 应用logo
     logo: "/static/logo.png",
     // 官方网站
-    site_url: "http://ruoyi.vip",
+    site_url: "http://xxx.vip",
     // 政策协议
     agreements: [{
         title: "隐私政策",
-        url: "https://ruoyi.vip/protocol.html"
+        url: "https://xxx.vip/protocol.html"
       },
       {
         title: "用户服务协议",
-        url: "https://ruoyi.vip/protocol.html"
+        url: "https://xxx.vip/protocol.html"
       }
     ]
   }
